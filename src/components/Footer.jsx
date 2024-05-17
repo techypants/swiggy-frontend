0e7df42246b98de899c5d04ff3effeafd0f13f45
techypants <tanmaycool2102@gmail.com>
2024-05-17 10:18:15 +0530

    fixed footer align

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="w-full flex items-center bg-[#050402] ">
-      <img
-        src="https://i.pinimg.com/564x/ae/f6/d0/aef6d084d09af1779ae1fb640d39a970.jpg"
-        alt="footer"
-        className="h-20 m-8"
-      ></img>
-      <span className="text-[#FFFFFF] m-[-40px] font-bold text-[25px]">
-        S W I G G Y
-      </span>
+    <div className="w-[100vw] bg-[#050402]">
+      <div className="w-[90%] m-auto flex items-center bg-[#050402] ">
+        <img
+          src="https://i.pinimg.com/564x/ae/f6/d0/aef6d084d09af1779ae1fb640d39a970.jpg"
+          alt="footer"
+          className="h-20 m-8"
+        ></img>
+        <span className="text-[#FFFFFF] m-[-40px] font-bold text-[25px]">
+          S W I G G Y
+        </span>
+      </div>
     </div>
   );
 }
