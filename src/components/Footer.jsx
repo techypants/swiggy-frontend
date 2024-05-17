84696f66128521f410b5eb2d6c18798f9d30f54a
techypants <tanmaycool2102@gmail.com>
2024-05-17 09:30:34 +0530

    fixed FOOTER

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="w-full bg-black ">
+    <div className="w-full flex items-center bg-[#050402] ">
       <img
-        src="https://pnggallery.com/wp-content/uploads/swiggy-logo-02.png"
+        src="https://i.pinimg.com/564x/ae/f6/d0/aef6d084d09af1779ae1fb640d39a970.jpg"
         alt="footer"
         className="h-28 m-8"
       ></img>
+      <span className="text-[#FFFFFF] m-[-40px] font-bold text-[40px]">
+        SWIGGY
+      </span>
     </div>
   );
 }
